a90a2ff9ad9120411ffef7f3824ec8aa3e6b8ff8
Ryanifar <ryanifar1414@gmail.com>
2022-03-24 23:41:00 +0800

    new comm

--- modul1/PRAK101.kt
+++ modul1/PRAK101.kt
@@ -1,10 +1,10 @@
 package modul1
 
 fun main(args: Array<String>){
-    print("Waktu Sekarang : "); val waktu = readLine()
-    print("Nama Anda : "); val nama = readLine()
-    print("Umur Anda : "); val umur = readLine()
-    print("Suhu Tubuh Anda : "); val suhu = readLine()
+    print("Waktu Sekarang : "); var waktu = readLine()
+    print("Nama Anda : "); var nama = readLine()
+    print("Umur Anda : "); var umur = readLine()
+    print("Suhu Tubuh Anda : "); var suhu = readLine()
 
     println("Selamat $waktu, $nama. \nUmur anda $umur Tahun.\nSuhu Tubuh anda $suhu derajat celcius")
 }
