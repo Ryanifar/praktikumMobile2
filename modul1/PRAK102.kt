c50a711daf1b273914d5065daa959135b77fc6d7
Ryanifar <ryanifar1414@gmail.com>
2022-03-24 23:37:16 +0800

    new comm

--- modul1/PRAK102.kt
+++ modul1/PRAK102.kt
@@ -1,13 +1,14 @@
 package modul1
-
 import java.util.Scanner;
 
-fun main(args: Array<String>){
-    val input = Scanner(System.`in`)
+fun matematika(x: Int): Int { var fx =
+    2*(Math.pow(x.toDouble(),2.0).toInt())+(5*x) - 8
+    return fx
+}
 
+fun main(args: Array<String>) {
+    var input = Scanner(System.`in`)
     print("Nilai x = ")
-    var x = input.nextInt()
-
-    val fx = ((2*(x*x))+(5*x)-8)
-    print("$fx")
+    var x: Int = input.nextInt()
+    println(matematika(x))
 }
--- modul1/PRAK103.kt
+++ modul1/PRAK103.kt
@@ -1,16 +1,17 @@
 package modul1
-
 import java.util.Scanner
 
-fun main(){
-    val input=Scanner(System.`in`)
-    println("Input Bilangan = ")
-    val bil = input.nextInt()
-    var i:Int=bil
-    for (i in 1..8) {
-        var hasil = bil * i
-        if(hasil%2 == 0 || hasil%3 == 0) {
-            print("$hasil ")
+fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    print("Input Bilangan = ")
+    var bilangan = input.nextInt()
+    var loop: Int = 1
+    var hasil: Int = 0
+
+    while(loop < 6 ) {
+        hasil = hasil + bilangan
+        if (hasil % 2 == 0 || hasil % 3 == 0) { print("$hasil ")
+            loop+=1
         }
     }
 }
